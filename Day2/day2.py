4435fadb918b851113405f86f6e3ccf7f50b49e0
Arthur Cluet <42556563+arthurcluet@users.noreply.github.com>
2023-04-26 14:34:05 +0200

    Day 2 Part 2

--- Day2/day2.py
+++ Day2/day2.py
@@ -12,10 +12,43 @@ def score(l):
     return score
 
 
+def part2(l):
+    [a, b] = l.split(' ')
+    toplay = {
+        "A": {
+            "X": 3,
+            "Y": 1,
+            "Z": 2
+        },
+        "B": {
+            "X": 1,
+            "Y": 2,
+            "Z": 3
+        },
+        "C": {
+            "X": 2,
+            "Y": 3,
+            "Z": 1
+        }
+    }
+    score = 0
+    if b == "X":
+        # need to lose
+        score += 0
+    elif b == "Y":
+        score += 3
+    elif b == "Z":
+        # need to win
+        score += 6
+    score += toplay[a][b]
+    return score
+
+
 def main():
     f = open("input.txt", "r")
     lines = f.read().splitlines()
-    print(sum(map(score, lines)))
+    print("Part 1:", sum(map(score, lines)))
+    print("Part 2:", sum(map(part2, lines)))
 
 
 if __name__ == "__main__":
